27f0ac8138d6525bc7adf0ad9c54abcc6f73ea06
Jelmer Poelstra <jelmerpoelstra@gmail.com>
2022-01-20 16:53:14 -0500

    Add trinotate step

--- workflow/setup.sh
+++ workflow/setup.sh
@@ -1,31 +1,48 @@
-#!/bin/bash
-
-## Create Conda environments
-mamba env create --force --file envs/rcorrector.yml
-mamba env create --force --file envs/trinity.yml
-mamba env create --force --file envs/busco.yml
-mamba env create --force --file envs/cd-hit.yml
-mamba env create --force --file envs/transrate.yml
-mamba env create --force --file envs/detonate.yml 
-
-## Bellepheron
-cd software
-git clone https://github.com/JesseKerkvliet/Bellerophon
-conda env create --file Bellerophon/envs/Bellerophon.yml
-cp Bellerophon/Bellerophon.py /users/PAS0471/jelmer/miniconda3/envs/bellerophon/bin/
-
-## Transrate
-https://github.com/blahah/transrate/issues/218
-
 ## Transfer files
-rsync -avz jp:/fs/project/PAS0471/jelmer/assist/2021-09_nisha/results /home/jelmer/Dropbox/mcic/assist/2021-09_nisha
-rsync -avz /home/jelmer/Dropbox/mcic/assist/2021-09_nisha/refdata/*gz jp:/fs/project/PAS0471/jelmer/assist/2021-09_nisha/refdata
+# rsync -avz jp:/fs/project/PAS0471/jelmer/assist/2021-09_nisha/results /home/jelmer/Dropbox/mcic/assist/2021-09_nisha
+# rsync -avz jp:/fs/project/PAS0471/jelmer/assist/2021-09_nisha/results/kallisto /home/jelmer/Dropbox/mcic/assist/2021-09_nisha/results
+# rsync -avz jp:/fs/project/PAS0471/jelmer/assist/2021-09_nisha/results/trinotate/GENE_TRANS_MAP /home/jelmer/Dropbox/mcic/assist/2021-09_nisha/results/trinotate/
+# rsync -avz /home/jelmer/Dropbox/mcic/assist/2021-09_nisha/refdata/*gz jp:/fs/project/PAS0471/jelmer/assist/2021-09_nisha/refdata
 
-## Create software environments
-module load python/3.6-conda5.2
-conda create -n trimgalore-env -y -c bioconda trim-galore=0.6.7
-#conda create -p /users/PAS0471/jelmer/.conda/envs/trimgalore-env -c bioconda trim-galore=0.6.7
 
+# SOFTWARE ---------------------------------------------------------------------
+## Create Conda environments
+mamba env create -f envs/rcorrector.yml
+mamba env create -f envs/trinity.yml
+mamba env create -f envs/busco.yml
+mamba env create -f envs/cd-hit.yml
+mamba env create -f envs/transrate.yml
+mamba env create -f envs/detonate.yml 
+mamba env create -f envs/trinonate.yml
+mamba env create -f envs/kallisto.yml
+
+## Install TRINOTATE dependencies into conda environment
+cd software
+wget https://services.healthtech.dtu.dk/download/b3dac17d-cc0b-4cf8-9ee7-92aafe1cb536/signalp-4.1g.Linux.tar.gz
+tar -xzvf signalp-4.1g.Linux.tar.gz
+cp signalp-4.1/signalp /users/PAS0471/jelmer/miniconda3/envs/trinotate-env/bin/
+
+wget https://services.healthtech.dtu.dk/download/3c40df53-f9c2-47c2-80a3-798c06b5cf85/tmhmm-2.0c.Linux.tar.gz
+tar -xzvf tmhmm-2.0c.Linux.tar.gz
+cp tmhmm-2.0c/bin/* /users/PAS0471/jelmer/miniconda3/envs/trinotate-env/bin/
+
+wget https://services.healthtech.dtu.dk/download/50fa2067-d5b4-409d-93d9-178b011680ed/rnammer-1.2.src.tar.gz
+tar -xzvf rnammer-1.2.src.tar.gz
+cp rnammer/*rnammer /users/PAS0471/jelmer/miniconda3/envs/trinotate-env/bin/
+
+wget http://eddylab.org/software/hmmer/hmmer-2.3.tar.gz
+tar -xzvf hmmer-2.3.tar.gz
+cd hmmer-2.3
+./configure
+make
+mv src/hmmsearch src/hmmsearch2
+
+env_dir=/users/PAS0471/jelmer/miniconda3/envs/trinotate-env
+mkdir "$env_dir"/bin/util/
+cp "$env_dir"/bin/superScaffoldGenerator.pl "$env_dir"/bin/util/  # Trinotate will look for it in util
+
+
+# DATA AND REF DATA ------------------------------------------------------------
 ## Download the FASTQ files from the NCSU sequencing core website
 dir_fq=data/fastq
 URL=http://lennon.gnets.ncsu.edu/Patwa/Patwa_NVS112A_fastqfiles.tar
@@ -38,3 +55,9 @@ rmdir "$dir_fq"/Patwa_fastqfiles
 ## Download the Ambrosiella cleistominuta reference genome
 URL=https://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/017/139/545/GCA_017139545.1_ASM1713954v1/GCA_017139545.1_ASM1713954v1_genomic.fna.gz
 wget "$URL" -P results/refdata/ambro_cleistominuta/
+
+
+# DOCS -------------------------------------------------------------------------
+## Assembly
+#? https://informatics.fas.harvard.edu/best-practices-for-de-novo-transcriptome-assembly-with-trinity.html
+#? https://www.protocols.io/view/de-novo-transcriptome-assembly-workflow-ghebt3e
--- workflow/workflow.sh
+++ workflow/workflow.sh
@@ -24,7 +24,13 @@ dir_cdhit=results/cdhit
 dir_busco=results/busco
 dir_rnaquast=results/rnaquast
 dir_detonate=results/detonate
+dir_kallisto=results/kallisto/beetle
+dir_trinotate=$PWD/results/trinotate
 
+assembly_id=trinity_cl0.95
+assembly=$PWD/"$dir_cdhit"/trinity_cl0.95.fa       # Abs path needed for Trinotate
+
+kallisto_idx="$dir_kallisto"/"$assembly_id".idx
 readstats=results/qc_misc/"$subvar"readstats.txt
 
 ## Reference data
@@ -38,18 +44,16 @@ ref_ambrox_idx=results/refdata/ambro_xylebori/star_idx
 ## Settings
 idx_size_fun=14      # STAR index size for combined fungal ref "genome" -- grep -v "^>" $fungal_refmix | wc -c # 748503311 => log2(748503311)/2 - 1) = 13.74 = 14 
 idx_size_ambro=11    # STAR index size for Ambrosiella ref genome -- zgrep -v "^>" $ref_ambro | wc -c # 27541947 => log2(27541947)/2 - 1) = 11.35 = 11
-minqual=5            # Min. qual for TrimGalore trimming - follows https://informatics.fas.harvard.edu/best-practices-for-de-novo-transcriptome-assembly-with-trinity.html
+minq=5               # Min. qual for TrimGalore trimming - follows https://informatics.fas.harvard.edu/best-practices-for-de-novo-transcriptome-assembly-with-trinity.html
 minlen=36            # Min. read length for TrimGalore trimming - follows https://informatics.fas.harvard.edu/best-practices-for-de-novo-transcriptome-assembly-with-trinity.htmlq
 mapmax_fun=100       # Max. nr of multimapping for fungal (pre-)mapping
 
+config_trinotate=$PWD/workflow/config/trinotate_conf.txt
+
 
 # PREPROCESSING ----------------------------------------------------------------
-## Subset FASTQ files for testing
-bash mcic-scripts/misc/subsample_fq_dir.sh -i data/fastq -o data/fastq/subset -n 100000
-
 ## Run FastQC
 for fastq_file in "$dir_fq"/*fastq.gz; do
-    echo -e "\nFASTQ file: $fastq_file"
     sbatch "$scr_mqc" "$fastq_file" "$dir_fqc"/raw
 done
 
@@ -61,18 +65,17 @@ sbatch scripts/rcorrector.sh "$dir_fq" "$dir_rcorr"
 
 ## Remove reads with unfixable errors, according to rcorrector
 for R1 in "$dir_rcorr"/*R1*.cor.fq.gz; do
-    sbatch scripts/readfilter.sh "$R1" "$dir_readfilt"
+    sbatch scripts/rcorrfilter.sh "$R1" "$dir_readfilt"
 done
 
 ## Run TrimGalore
 for R1 in "$dir_readfilt"/*R1*fastq.gz; do
-    sbatch "$scr_trimg" -i "$R1" -o "$dir_trim"/rcorr \
-        -O "$dir_fqc"/trimmed_rcorr -q "$minqual" -l "$minlen"
+    sbatch "$scr_trimg" -i "$R1" -o "$dir_trim"/rcorr -O "$dir_fqc"/trimmed_rcorr -q "$minq" -l "$minlen"
 done
 for R1 in "$dir_fq"/*R1*fastq.gz; do
-    sbatch "$scr_trimg" -i "$R1" -o "$dir_trim"/nocorr \
-        -O "$dir_fqc"/trimmed_nocorr -q "$minqual" -l "$minlen"
+    sbatch "$scr_trimg" -i "$R1" -o "$dir_trim"/nocorr -O "$dir_fqc"/trimmed_nocorr -q "$minq" -l "$minlen"
 done
+
 
 # MAP TO FUNGAL GENOMES --------------------------------------------------------
 ## Download fungal seqs and create indices
@@ -98,59 +101,78 @@ done
 bash scripts/readstats.sh "$dir_trim" "$dir_map2fun" "$dir_map2ambrox" "$readstats"
 
 
-# ASSEMBLE BEETLE READS --------------------------------------------------------
-#> https://informatics.fas.harvard.edu/best-practices-for-de-novo-transcriptome-assembly-with-trinity.html
-
+# BEETLE TRANSCRIPTOME ASSEMBLY ------------------------------------------------
 ## Assemble with Trinity
 sbatch scripts/trinity.sh "$dir_map2fun"/rcorr/unmapped "$dir_trinity_scratch"
 cp "$dir_trinity_scratch"/Trinity.fasta "$dir_trinity"
 
 ## Redundant transcript removal
-sbatch scripts/cd-hit.sh $dir_trinity/Trinity.fasta $dir_cdhit/trinity_cl0.95.fa
+sbatch scripts/cd-hit.sh $dir_trinity/Trinity.fasta "$assembly"
 
-## Run Trinity-stats - https://informatics.fas.harvard.edu/best-practices-for-de-novo-transcriptome-assembly-with-trinity.html
+
+# BEETLE ASSEMBLY QC -----------------------------------------------------------
+## Run Trinity-stats
 TrinityStats.pl $dir_trinity/Trinity.fasta > "$dir_trinity"/Trinity_assembly.metrics
 
 ## rnaQUAST
-sbatch scripts/rnaquast.sh $dir_cdhit/trinity_cl0.95.fa "$dir_rnaquast"
+sbatch scripts/rnaquast.sh "$assembly" "$dir_rnaquast"
 
 ## Run BUSCO
 busco_db=insecta_odb10
-sbatch scripts/busco.sh "$dir_cdhit"/trinity_cl0.95.fa "$dir_busco" "$busco_db"
+sbatch scripts/busco.sh "$assembly" "$dir_busco" "$busco_db"
 mv busco_*log "$dir_busco"; mv busco_downloads "$dir_busco"
-        # --------------------------------------------------
-        # |Results from dataset insecta_odb10               |
-        # --------------------------------------------------
-        # |C:93.6%[S:57.8%,D:35.8%],F:2.6%,M:3.8%,n:1367    |
-        # |1279   Complete BUSCOs (C)                       |
-        # |790    Complete and single-copy BUSCOs (S)       |
-        # |489    Complete and duplicated BUSCOs (D)        |
-        # |35     Fragmented BUSCOs (F)                     |
-        # |53     Missing BUSCOs (M)                        |
-        # |1367   Total BUSCO groups searched               |
 
 ## Run Detonate
-sbatch scripts/detonate.sh "$dir_cdhit"/trinity_cl0.95.fa "$dir_trim"/rcorr "$dir_detonate"
+sbatch scripts/detonate.sh "$assembly" "$dir_trim"/rcorr "$dir_detonate"
 
 ## Quantify read support by mapping with Bowtie - https://informatics.fas.harvard.edu/best-practices-for-de-novo-transcriptome-assembly-with-trinity.html
+#> https://github.com/trinityrnaseq/trinityrnaseq/wiki/RNA-Seq-Read-Representation-by-Trinity-Assembly
 # bowtie2-build -–threads 4 $1 $assembly_prefix
 # bowtie2 -p 10 -q --no-unal -k 20 -x $1 -1 $2 -2 $3  2>align_stats.txt| ${SINGULARITY_EXEC} samtools view -@10 -Sb -o bowtie2.bam
+#? A typical Trinity transcriptome assembly will have the vast majority of all reads mapping back to the assembly, and ~70-80% of the mapped fragments found mapped as proper pairs 
 
-#! https://www.protocols.io/view/de-novo-transcriptome-assembly-workflow-ghebt3e?step=15
+## Count full-length transcripts?
+#> https://github.com/trinityrnaseq/trinityrnaseq/wiki/Counting-Full-Length-Trinity-Transcripts
 
-#TODO - Filter using expression level https://www.biorxiv.org/content/10.1101/035642v2.full ?
-#> In general, for low coverage datasets (less than 20 million reads), filtering based on expression,
-#> using TPM=1 as a threshold performs well, with Transrate filtering being too aggressive.
-#> With higher coverage data (more than 60 million reads) Transrate filtering may be optimal, as may gene expression filtering using a threshold of TPM=0.5.
 
-## Another round of fungal sequence removal?
-#> See https://www.ncbi.nlm.nih.gov/pmc/articles/PMC6463014/, which used competitive BlastP
+# BEETLE ASSEMBLY FILTERING ----------------------------------------------------
+## - Another round of fungal sequence removal? See https://www.ncbi.nlm.nih.gov/pmc/articles/PMC6463014/, which used competitive BlastP
+## - Can try TPM>1 filter using Detonate results, as in [Error, noise and bias in de novo transcriptome assemblies](https://onlinelibrary.wiley.com/doi/10.1111/1755-0998.13156)
+## - Can try BlastP evalue < 1e-5 as in [Error, noise and bias in de novo transcriptome assemblies](https://onlinelibrary.wiley.com/doi/10.1111/1755-0998.13156)
+## - Try DRAP? http://www.sigenae.org/drap/install.html
 
 
 # BEETLE ASSEMBLY ANNOTATION ---------------------------------------------------
+#? Try dammit? https://angus.readthedocs.io/en/2017/dammit_annotation.html#
+
+sbatch scripts/trinotate.sh "$assembly" "$config_trinotate" "$dir_trinotate"
 
 
-# AMBROSIELLA ------------------------------------------------------------------
+# GENE AND TRANSCRIPT QUANTIFICATION -------------------------------------------
+#? https://github.com/trinityrnaseq/trinityrnaseq/wiki/Trinity-Transcript-Quantification
+
+## Kallisto
+sbatch mcic-scripts/rnaseq/kallisto_index.sh -i "$assembly" -o "$kallisto_idx"
+for R1 in "$dir_trim"/nocorr/*R1*fastq.gz; do
+    sbatch mcic-scripts/rnaseq/kallisto_quant.sh -i "$R1" -o "$dir_kallisto" -r "$kallisto_idx" 
+done
+
+# https://www.nature.com/articles/s41598-019-44499-3#Sec2
+#> Subsequently, the expression abundance for each contig was estimated using one alignment-based and two alignment-free quantifiers,
+#> namely (1) Bowtie2 (ver. 2.3.0)38 (–dpad 0–gbar 99999999–mp 1,1–np 1–score-min L,0,-0,1 -k 200–sensitive–no-mixed–no-discordant) followed by RSEM (ver. 1.2.31) (default parameters);
+#> (2) Kallisto (ver. 0.43.0) (indexing with -k 31 and quantifying with default parameters); and (3) Salmon (ver. 0.8.2) (indexing with -k 31 and quantifying with default parameters).
+
+# DIFFERENTIAL EXPRESSION ------------------------------------------------------
+
+## Gene-to-transcript map: results/trinotate/GENE_TRANS_MAP
+
+#? Try Ballgown? https://www.bioconductor.org/packages/devel/bioc/vignettes/ballgown/inst/doc/ballgown.html
+
+
+#TODO
+#> https://link.springer.com/protocol/10.1007%2F978-1-0716-1609-3_8#Sec6
+
+# ASSEMBLE AMBROSIELLA TRANSCRIPTOME -------------------------------------------
 ## Assemble
 #> https://github.com/trinityrnaseq/trinityrnaseq/wiki/Running-Trinity
 #> If your transcriptome RNA-seq data are derived from a gene-dense compact genome, such as from fungal genomes,
